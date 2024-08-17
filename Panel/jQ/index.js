63afd4f7fb5b37f94f4e84183be8303994ab89a1
dev.swarup <dev.swarup@hotmail.com>
2024-08-17 11:17:42 +0530

    Removed the `Worker` method, and applied the `C++` Mutex

--- Panel/jQ/index.js
+++ Panel/jQ/index.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const { Worker } = require("node:worker_threads");
 
 const jQ = require(path.join(__dirname, "mem.node"));
 const jQFast = require(path.join(__dirname, "mem.scan.node"));
@@ -16,43 +15,23 @@ module.exports.FindEmulator = () => {
 
             case "HD-Player.exe":
                 return { pid: th32ProcessID, name: "BlueStacks" };
-
-            case "MEmuHeadless.exe":
-                return { pid: th32ProcessID, name: "Memu" };
-
-            case "NoxVMHandle.exe":
-                return { pid: th32ProcessID, name: "Nox Player" };
-
-            case "LdVBoxHeadless.exe":
-                return { pid: th32ProcessID, name: "LD Player" };
         };
     }).filter(i => i);
 
     return result.length > 0 ? result : false;
 };
 
-module.exports.FindInGame = async (handle, scanValue) => new Promise(resolve => {
-    try {
-        resolve(jQFast.fastFindPattern(handle, scanValue.split(" ").map(i => i !== "??" ? i : "?").join(" ")));
-    } catch { resolve([]); };
-});
-
 module.exports.InjectFile = (pid, path) => {
     const { handle } = jQ.openProcess(pid);
     return new Promise(resolve => { try { jQ.injectDll(handle, path); setTimeout(() => resolve(true), 1800); } catch (err) { console.log(err); resolve(false); } });
 };
 
+const FindInGame = async (handle, scanValue) => new Promise(resolve =>
+    jQFast.fastFindPattern(handle, scanValue.split(" ").map(i => i !== "??" ? i : "?").join(" "), resolve));
 
 module.exports.AsyncFindValues = (pid, scanValue) => new Promise((resolve, reject) => {
-    const { handle } = jQFast.openProcess(pid); const proc = new Worker(`
-        require("bytenode");
-        const jQ = require("${path.join(__dirname, "index.jsc").replaceAll("\\", "/")}");
-        
-        jQ.FindInGame(${handle}, "${scanValue}").then(address => require("node:worker_threads").parentPort.postMessage(address));
-    `, { eval: true });
-
-    proc.on("error", reject);
-    proc.on("message", resolve);
+    const { handle } = jQFast.openProcess(pid);
+    FindInGame(handle, scanValue).then(resolve).catch(reject);
 });
 
 module.exports.InjectValues =
