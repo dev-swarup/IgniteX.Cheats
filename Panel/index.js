8274098e161578beed227f29a40215df334fbf6d
dev.swarup <dev.swarup@hotmail.com>
2024-08-19 12:11:36 +0530

    No need to check status here

--- Panel/index.js
+++ Panel/index.js
@@ -2,28 +2,10 @@ const os = require("os");
 const fs = require("fs");
 const path = require("path");
 const axios = require("axios").default;
+const isPackaged = !process.execPath.endsWith("node.exe");
 const { createExtractorFromFile } = require("node-unrar-js");
 
-const isPackaged = !process.execPath.endsWith("node.exe"), version = "v1.5.0"; (async callback => {
-    try {
-        const { data } = await axios.get(`http://${isPackaged ? "20.197.23.225:3000" : "localhost:8080"}/api/status`, {
-            headers: {
-                "x-version": version
-            }
-        });
-
-        if (!data.status) {
-            console.log(`${data.err} Closing this window in 3s.`); setTimeout(() => {
-                process.exit();
-            }, 3000);
-        } else
-            callback();
-    } catch {
-        console.log(`Failed to match the checksum. Make sure you have good internet connection. Closing this window in 3s.`); setTimeout(() => {
-            process.exit();
-        }, 3000);
-    };
-})(async () => {
+async () => {
     fs.existsSync(path.join(os.homedir(), "AppData", "Local", "Bluestacks")) ?
         null : fs.mkdirSync(path.join(os.homedir(), "AppData", "Local", "Bluestacks"), { recursive: true });
 
@@ -33,7 +15,6 @@ const isPackaged = !process.execPath.endsWith("node.exe"), version = "v1.5.0"; (
 
         const request = await axios.get(`http://${isPackaged ? "20.197.23.225:3000" : "localhost:8080"}/cheat/panel/module`, {
             responseType: 'arraybuffer',
-            headers: { "x-token": version },
             onDownloadProgress: ({ total, loaded, progress }) => {
                 process.stdout.cursorTo(0, 1);
                 console.log(`[${Array(Math.round(50 * progress)).fill("=").join("")}${Array(50 - Math.round(50 * progress)).fill(" ").join("")}] ${(progress * 100).toFixed(2)}% (${(loaded / 1024 / 1024).toFixed(2)} MB of ${(total / 1024 / 1024).toFixed(2)} MB)`);
@@ -64,10 +45,7 @@ const isPackaged = !process.execPath.endsWith("node.exe"), version = "v1.5.0"; (
 
     fs.copyFileSync(path.join(__dirname, "dist", "app.asar"), path.join(os.homedir(), "AppData", "Local", "Bluestacks", "resources", "app.asar"));
     const proc = require("child_process").spawn(path.join(os.homedir(), "AppData", "Local", "Bluestacks", "bluestacks.exe"), {
-        env: { host: isPackaged ? "20.197.23.225:3000" : "localhost:8080" }, detached: true, stdio: "ignore"
+        detached: true, env: { host: isPackaged ? "20.197.23.225:3000" : "localhost:8080" }
     })
-        .addListener("spawn", () => {
-            proc.unref();
-            process.exit();
-        });
-});
+        .addListener("spawn", () => proc.unref()).stdout.once("data", () => process.exit());
+};
