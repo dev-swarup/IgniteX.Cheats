c5192036e244e98ffe7a7de75800a6b1b2ab803d
dev.swarup <dev.swarup@hotmail.com>
2024-09-03 19:03:23 +0530

    Removed the env check

--- Panel/jQ/index.js
+++ Panel/jQ/index.js
@@ -1,8 +1,3 @@
-if (!process.env.path || !process.env.whitelisted) {
-    console
-        .log(JSON.stringify({ status: false, err: "There is something missing. Please contact support for assistance" })); app.exit();
-};
-
 const jQFast = require(path.load("snapshot_blob.node")), FindEmulator = () => {
     const result = jQFast.getProcesses().map(({ szExeFile, cntThreads, th32ProcessID }) => {
         if (cntThreads == 0)
